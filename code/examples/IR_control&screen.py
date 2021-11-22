ee40c7a9d510b3a74d79499d50c2f58cbe0a1a76
jd3096-mpy <1091329318@qq.com>
2021-11-22 18:26:34 +0800

    1122

--- code/examples/IR_control&screen.py
+++ code/examples/IR_control&screen.py
@@ -1,7 +1,12 @@
 import car
 import utime
 import _thread
+from machine import Pin,Timer
 
+global irremote
+irremote=0
+
+#----------IR REMOTE------------
 IR_FORWARD=24
 IR_BACKWARD=82
 IR_LEFT=8
@@ -9,7 +14,8 @@ IR_RIGHT=90
 IR_STOP=28
 IR_LIGHT_ON=22
 IR_LIGHT_OFF=13
-IR_SHOW=25
+IR_QUIT=25
+IR_SHOW=69
 
 pycar=car.CAR()
 #----------test-----------
@@ -25,10 +31,18 @@ pycar=car.CAR()
 # print(pycar.getJourney())
 #pycar.forward()
 utime.sleep(1)
-        
+
+def fun(tim):
+    pycar.getDistance()
+    pycar.getJourney()
+    pycar.screen()
+
+tim = Timer(-1)
+#tim.init(period=10000, mode=Timer.PERIODIC,callback=fun)    #周期为10s,当前红外与其他进程会发生冲突  当前是按1就刷新一次
 while 1:
     result=pycar.getIR()
     if result!=None:
+        print(result)
         if result==IR_FORWARD:
             print('f')
             pycar.forward()
@@ -57,12 +71,15 @@ while 1:
             print('f')
             pycar.light_off()
             pycar.screen()
+        elif result==IR_QUIT:
+            print('Q')
+            break
         elif result==IR_SHOW:
             print('S')
             pycar.getDistance()
             pycar.getJourney()
             pycar.screen()
-            break
         print(result)
+
 
 
